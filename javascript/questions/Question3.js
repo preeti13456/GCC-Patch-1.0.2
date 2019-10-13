848a620cb29b9d9f55434760ed0181da8cd4591b
preeti13456 <36129347+preeti13456@users.noreply.github.com>
2019-10-13 22:54:17 +0530

    Update Question3.js

--- javascript/questions/Question3.js
+++ javascript/questions/Question3.js
@@ -1,6 +1,25 @@
 // Use this file to answer question 3
 
 function answer03(scores, alice) {
+    function Q3Object() {
+    }
+
+    function getScores() {
+        return scores;
+    }
+
+    function setScores(scores) {
+        this.scores = scores;
+    }
+
+    function getAlice() {
+        return alice;
+    }
+
+    function setAlice(alice) {
+        this.alice = this.alice;
+    }
+
     return -5
 }
 
