c9fd051b720805345a7f955547e8273c843adf50
preeti13456 <36129347+preeti13456@users.noreply.github.com>
2019-10-13 21:31:39 +0530

    Update Question1.js
    First time conversion of java code to js code

--- javascript/questions/Question1.js
+++ javascript/questions/Question1.js
@@ -1,7 +1,37 @@
 // Use this file to answer question 1
 
 function answer01(initialLevelOfDebt, interestPercentage, repaymentPercentage) {
+    
+     function Q1Object() {
+    }
+
+    function  getInitialLevelOfDebt() {
+        return initialLevelOfDebt;
+    }
+
+    function setInitialLevelOfDebt(initialLevelOfDebt) {
+        this.initialLevelOfDebt = initialLevelOfDebt;
+    }
+
+   function  getRepaymentPercentage() {
+        return repaymentPercentage;
+    }
+
+    function setRepaymentPercentage(repaymentPercentage) {
+        this.repaymentPercentage = repaymentPercentage;
+    }
+
+    function getInterestPercentage() {
+        return interestPercentage;
+    }
+
+    function setInterestPercentage(interestPercentage) {
+        this.interestPercentage = interestPercentage;
+    }
+
+
     return -5
-}
+
 
 module.exports = answer01
+}
