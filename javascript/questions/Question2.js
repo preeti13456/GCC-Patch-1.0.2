9f9aaf02b76aa617301b8056f29feb254c64913d
preeti13456 <36129347+preeti13456@users.noreply.github.com>
2019-10-13 22:24:36 +0530

    Update Question2.js

--- javascript/questions/Question2.js
+++ javascript/questions/Question2.js
@@ -1,6 +1,34 @@
 // Use this file to answer question 2
 
 function answer02(risk, bonus, trader) {
+    
+    function Q2Object() {
+    }
+
+    function getRisk() {
+        return risk;
+    }
+
+    function setRisk(risk) {
+        this.risk = risk;
+    }
+
+    function getBonus() {
+        return bonus;
+    }
+
+    function setBonus(bonus) {
+        this.bonus = bonus;
+    }
+
+    function getTrader() {
+        return trader;
+    }
+
+    function setTrader(trader) {
+        this.trader = trader;
+    }
+
     return -5
 }
 
