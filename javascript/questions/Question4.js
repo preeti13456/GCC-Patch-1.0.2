f2311ab841567b71ad40c6107b23d01022afd8f5
preeti13456 <36129347+preeti13456@users.noreply.github.com>
2019-10-13 23:18:37 +0530

    Update Question4.js

--- javascript/questions/Question4.js
+++ javascript/questions/Question4.js
@@ -1,6 +1,32 @@
 // Use this file to answer question 4
 
 function answer04(v, c, mc) {
+    function Q4Object() {
+    }
+
+    function getV() {
+        return v;
+    }
+
+    function setV(v) {
+        this.v = v;
+    }
+
+    function getC() {
+        return c;
+    }
+
+    function setC(c) {
+        this.c = c;
+    }
+
+    function getMc() {
+        return mc;
+    }
+
+    function setMc(mc) {
+        this.mc = mc;
+    }
     return -5
 }
 
